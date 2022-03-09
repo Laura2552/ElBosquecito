ac84323eb057e3ee63fba9231453bfa295244362
Laura Lajara <Lajaralaura@outlook.es>
2022-03-08 22:03:22 -0400

    Creacion y conexion c/base de datos

---
+++ ElBosquecito/ElBosquecito/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElBosquecito.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
